2a0519b69a1287ca28e0fcaf42b606b3a74a3221
aradenkodrat <101814131+aradenkodrat@users.noreply.github.com>
2022-07-18 22:08:26 +0700

    Update login.php

--- login.php
+++ login.php
@@ -35,7 +35,7 @@ if (!isset($_SESSION['log'])){
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>Login</title>
+    <title>Login klinik_312010271</title>
     <link href="css/styles.css" rel="stylesheet" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" crossorigin="anonymous"></script>
 </head>
@@ -47,7 +47,7 @@ if (!isset($_SESSION['log'])){
                     <div class="row justify-content-center">
                         <div class="col-lg-5">
                             <div class="card shadow-lg border-0 rounded-lg mt-5">
-                                <div class="card-header"><h3 class="text-center font-weight-light my-4">Login</h3></div>
+                                <div class="card-header"><h3 class="text-center font-weight-light my-4">Login klinik_312010271</h3></div>
                                 <div class="card-body">
                                     <form method="post">
                                         <div class="form-group">
